14ed60ec5fb9ca559c4663b23f33ec7015e9b5ea
ikcam <me@ikcam.com>
2016-09-04 15:52:57 -0500

    requirements

--- setup.py
+++ setup.py
@@ -33,29 +33,31 @@ setup(
     author='Cubo',
     author_email='somos@cubo.pe',
     url='https://github.com/cubope/python-easyengine',
+    keywords=['python', 'easyengine', 'management'],
     install_requires = [
         'paramiko>=2.0.2'
     ],
+    extras_require={
+        ':python_version=="3.2"': ['paramiko'],
+        ':python_version=="3.3"': ['paramiko'],
+        ':python_version=="3.4"': ['paramiko'],
+        ':python_version=="3.5"': ['paramiko'],
+    },
     packages=[
         'easyengine',
     ],
     include_package_data=True,
-    extras_require={
-    },
     license="Apache License 2.0",
-    zip_safe=False,
-    keywords=['python', 'easyengine', 'management'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries',
         'Topic :: Utilities',
         'Environment :: Web Environment',
